cc3639eb3e46c4f0f6e7ebfd3537cd63e0dbc10a
김영욱 <ziziza93@naver.com>
2021-12-30 22:33:31 +0900

    컴포넌트(클래스형)의 LifeCycle
    constructor -> state 초기화 및 메서드 바인딩
    componentDidMount -> Dom 노드 초기화 및 데이터 fetch / 최초 렌더 후 실행
    componentWillUnmount -> 타이머 제거 및 요청 취소, 구독해제 등 / 컴포넌트가 사라질 때 실행
    
    Functional Component에서는 hook으로 거의 구현함

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
-import Composition from './components/2-4.Props/Composition';
-import Extraction from './components/2-4.Props/Extraction/Extraction';
-import ClassComponent from './components/2-5.State/ClassComponent';
-import FunctionalComponent from './components/2-5.State/FunctionalComponent';
+import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent';
 
 function App() {
 	return (
 		<div className="App">
-			<FunctionalComponent/>
-			<ClassComponent/>
-			<Extraction/>
-			<Composition/>
+			<ClassComponent2/>
+			{/*<FunctionalComponent/>*/}
+			{/*<ClassComponent/>*/}
+			{/*<Extraction/>*/}
+			{/*<Composition/>*/}
 		</div>
 	);
 }
---
+++ src/components/2-6.LifeCycle/ClassComponent.jsx
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+
+class ClassComponent extends Component {
+	constructor(props) {
+		super(props);
+		console.log('constructor');
+		this.state = {date: new Date()};
+		this.handleClick = this.handleClick.bind(this);
+	}
+	componentDidMount() {
+		console.log('componentDidMount');
+		this.timerID = setInterval(() => {this.tick()}, 1000);
+	}
+
+	componentDidUpdate() {
+		console.log('componentDidUpdate');
+	}
+
+	componentWillUnmount() {
+		console.log('componentWillUnmount');
+		clearInterval(this.timerID);
+	}
+
+	tick() {
+		console.log('tick');
+		this.setState(({date: new Date()}))
+	}
+
+	handleClick() {
+		alert(this.state.date);
+	}
+
+	render() {
+		console.log('render')
+		return (
+			<div>
+				<h1 onClick={this.handleClick}>Hello, world!</h1>
+				<h2>{this.state.date.toLocaleTimeString()}</h2>
+			</div>
+		);
+	}
+}
+
+export default ClassComponent;
